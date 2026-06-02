7cfd7aa97d56590fbe6659ae2e956b4c10ff8b3b
RodneyCodess <rodneyga@buffalo.edu>
2026-06-02 13:02:41 -0400

    update

--- tests/ci/scripts/build_xdmod_img.sh
+++ tests/ci/scripts/build_xdmod_img.sh
@@ -6,9 +6,12 @@ XDMOD_VERSION="$1"
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_DIR=$BASE_DIR/../../..
 
-docker pull "tools-ext-01.ccr.xdmod.org/xdmod:x86_64-rockylinux8.9.20231119-v11.0.0-1.0-03"
-docker save -o "$PROJECT_DIR/$XDMOD_VERSION.tar" "tools-ext-01.ccr.xdmod.org/xdmod:x86_64-rockylinux8.9.20231119-v11.0.0-1.0-03"
-pwd && ls -la "$PROJECT_DIR"
+BASE_IMAGE = "tools-ext-01.ccr.xdmod.org/xdmod:x86_64-rockylinux8.9.20231119-v11.0.0-1.0-03"
+
+docker pull $"BASE_IMAGE"
+
+IMAGE_TO_SAVE="$BASE_IMAGE"
+
 
 if [[ "$XDMOD_VERSION" == "xdmod-main-dev" || "$XDMOD_VERSION" == "xdmod-11-0-dev" ]]; then
     if [ "$XDMOD_VERSION" == 'xdmod-main-dev' ]; then
@@ -16,6 +19,9 @@ if [[ "$XDMOD_VERSION" == "xdmod-main-dev" || "$XDMOD_VERSION" == "xdmod-11-0-de
     else
         branch="xdmod$(echo $XDMOD_VERSION | sed 's/xdmod-\(.*\)-dev/\1/' | sed 's/-/./')"
     fi
+
+    docker run -dt --name "$XDMOD_VERSION" -h "$XDMOD_VERSION" 
+        "$BASE_IMAGE"
 
     docker exec $XDMOD_VERSION bash -c "git clone --depth=1 --branch=$branch https://github.com/ubccr/xdmod.git /root/xdmod"
     docker exec -w /root/xdmod $XDMOD_VERSION bash -c 'composer install'
@@ -30,7 +36,12 @@ if [[ "$XDMOD_VERSION" == "xdmod-main-dev" || "$XDMOD_VERSION" == "xdmod-11-0-de
     fi
     docker exec -w /root/xdmod $XDMOD_VERSION bash -c 'XDMOD_TEST_MODE=upgrade bash -x ./tests/ci/bootstrap.sh'
     docker exec -w /root/xdmod $XDMOD_VERSION bash -c './tests/ci/validate.sh'
+
+    docker commit "$XDMOD_VERSION" "$XDMOD_VERSION:built"
+    IMAGE_TO_SAVE="$XDMOD_VERSION:built"
 fi
+
+docker save -o "$PROJECT_DIR/$XDMOD_VERSION.tar" "$IMAGE_TO_SAVE"
 # Update the server hostnames and certificates so the Python containers can
 # make requests to them.
 #docker exec $XDMOD_VERSION bash -c "sed -i 's/localhost/$XDMOD_VERSION/g' /etc/httpd/conf.d/xdmod.conf"
