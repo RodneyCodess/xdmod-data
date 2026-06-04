2c9f64f47b54432d0d501f8b5e5310168a806936
RodneyCodess <rodneyga@buffalo.edu>
2026-06-04 13:17:44 -0400

    update

--- tests/ci/scripts/run_tests.sh
+++ tests/ci/scripts/run_tests.sh
@@ -11,3 +11,14 @@ docker run -dt --name "$XDMOD_VERSION" -p 8080:443 "$loaded_image"
  
 echo "$loaded_image"
 
+# download python  with uv 
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source "$HOME/.local/bin/env"
+
+uv python install "$PYTHON_VERSION"
+uv venv --python "$PYTHON_VERSION"
+source .venv/bin/activate
+
+#check python version 
+python --version
