09842071a0d16dccd488be62e9fc5964569a65a6
RodneyCodess <rodneyga@buffalo.edu>
2026-06-08 12:00:15 -0400

    update

--- tests/ci/scripts/run_tests.sh
+++ tests/ci/scripts/run_tests.sh
@@ -6,7 +6,7 @@ docker load -i "$XDMOD_VERSION.tar" && docker images
 loaded_image="$(docker load -i "$XDMOD_VERSION.tar" | sed 's/Loaded image: //')"
 docker run -dt --name "$XDMOD_VERSION" -p 8080:443 "$loaded_image"
 
-docker exec $XDMOD_VERSION bash -c '~/root/bin/services start'
+docker exec $XDMOD_VERSION bash -c '/root/bin/services start'
 
 
 curl -k https://localhost:8080
