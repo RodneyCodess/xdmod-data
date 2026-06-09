f708c3f705a4edaf79b6dbbd457f249d9961ec06
RodneyCodess <rodneyga@buffalo.edu>
2026-06-09 09:48:07 -0400

    update

--- tests/ci/scripts/build_xdmod_img.sh
+++ tests/ci/scripts/build_xdmod_img.sh
@@ -13,8 +13,8 @@ docker pull "$BASE_IMAGE"
 IMAGE_TO_SAVE="$BASE_IMAGE"
 
 # Generate OpenSSL key and certificate.
-docker exec $XDMOD_VERSION bash -c "openssl genrsa -rand /proc/cpuinfo:/proc/filesystems:/proc/interrupts:/proc/ioports:/proc/uptime 2048 > /etc/pki/tls/private/localhost.key"
-docker exec $XDMOD_VERSION bash -c "openssl req -new -key /etc/pki/tls/private/localhost.key -x509 -sha256 -days 365 -set_serial $RANDOM -extensions v3_req -out /etc/pki/tls/certs/localhost.crt -subj '/C=XX/L=Default City/O=Default Company Ltd/CN=localhost' -addext 'subjectAltName=DNS:localhost'"
+#docker exec $XDMOD_VERSION bash -c "openssl genrsa -rand /proc/cpuinfo:/proc/filesystems:/proc/interrupts:/proc/ioports:/proc/uptime 2048 > /etc/pki/tls/private/localhost.key"
+#docker exec $XDMOD_VERSION bash -c "openssl req -new -key /etc/pki/tls/private/localhost.key -x509 -sha256 -days 365 -set_serial $RANDOM -extensions v3_req -out /etc/pki/tls/certs/localhost.crt -subj '/C=XX/L=Default City/O=Default Company Ltd/CN=localhost' -addext 'subjectAltName=DNS:localhost'"
     
 
 if [[ "$XDMOD_VERSION" == "xdmod-main-dev" || "$XDMOD_VERSION" == "xdmod-11-0-dev" ]]; then
