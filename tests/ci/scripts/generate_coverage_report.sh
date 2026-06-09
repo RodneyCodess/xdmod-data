0ee1dbf0784276745921234bcf180a3b1efe507e
RodneyCodess <rodneyga@buffalo.edu>
2026-06-09 13:05:14 -0400

    update

--- tests/ci/scripts/generate_coverage_report.sh
+++ tests/ci/scripts/generate_coverage_report.sh
@@ -3,7 +3,10 @@ set -exo pipefail
 
 
 if [ "$PYTHON_VERSION" = "min-python" ]; then
+    pyenv install -s 3.8
     pyenv global 3.8
+else
+    pyenv global 3.14
 fi
 
 python3 -m pip install --upgrade coverage
