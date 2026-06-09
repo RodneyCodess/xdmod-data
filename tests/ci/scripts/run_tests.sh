250f59fe4dffa9a19a4c0a149eaa402cf7a5657c
RodneyCodess <rodneyga@buffalo.edu>
2026-06-09 10:50:28 -0400

    update

--- tests/ci/scripts/run_tests.sh
+++ tests/ci/scripts/run_tests.sh
@@ -42,8 +42,7 @@ echo "XDMOD_API_TOKEN=$api_token" > ${XDMOD_VERSION}-token
 
 XDMOD_HOST="https://localhost:8080" python3 -m pytest --cov --cov-branch -vvs -o log_cli=true tests/
 
+mv .coverage ".coverage.${PYTHON_VERSION}.${XDMOD_VERSION}"
 
 python3 -m pip freeze
-
-
 
