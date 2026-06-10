f1593dbd312977d5dc41881830ccdb88592dce8f
RodneyCodess <rodneyga@buffalo.edu>
2026-06-10 10:14:23 -0400

    update

--- tests/ci/scripts/run_tests.sh
+++ tests/ci/scripts/run_tests.sh
@@ -48,7 +48,9 @@ api_token=$(curl -k -sS -X POST -b xdmod.cookie "https://localhost:8080/rest/use
 
 echo "XDMOD_API_TOKEN=$api_token" > ${XDMOD_VERSION}-token
 
-REQUESTS_CA_BUNDLE="$(pwd)/localhost.crt" CURL_CA_BUNDLE="$(pwd)/localhost.crt" XDMOD_HOST="https://localhost:8080" python3 -m pytest --cov --cov-branch -vvs -o log_cli=true tests/ || true
+echo " THIS IS THE API TOKEN !!!!! [$api_token]"
+
+REQUESTS_CA_BUNDLE="$(pwd)/localhost.crt" CURL_CA_BUNDLE="$(pwd)/localhost.crt" XDMOD_API_TOKEN="$api_token" XDMOD_HOST="https://localhost:8080" python3 -m pytest --cov --cov-branch -vvs -o log_cli=true tests/ || true
 
 mv .coverage ".coverage.${PYTHON_VERSION}.${XDMOD_VERSION}"
 
