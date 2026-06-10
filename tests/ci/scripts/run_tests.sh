cfb25ce746635968e366f1dcf3e1cea5149a6547
RodneyCodess <rodneyga@buffalo.edu>
2026-06-10 14:45:38 -0400

    update

--- tests/ci/scripts/run_tests.sh
+++ tests/ci/scripts/run_tests.sh
@@ -40,11 +40,11 @@ if [ "$PYTHON_VERSION" = "min-python" ]; then
 fi
 
 rest_token=$(
-    curl -k -sS -X POST -c xdmod.cookie -d 'username=normaluser&password=normaluser' https://localhost:8080/rest/auth/login | 
+    curl -sS -X POST -c xdmod.cookie -d 'username=normaluser&password=normaluser' https://localhost:8080/rest/auth/login | 
     jq -r '.results.token'
     )
 
-api_token=$(curl -k -sS -X POST -b xdmod.cookie "https://localhost:8080/rest/users/current/api/token?token=$rest_token" | jq -r '.data.token')
+api_token=$(curl -sS -X POST -b xdmod.cookie "https://localhost:8080/rest/users/current/api/token?token=$rest_token" | jq -r '.data.token')
 
 echo "XDMOD_API_TOKEN=$api_token" > ${XDMOD_VERSION}-token
 
@@ -54,7 +54,7 @@ ls -la localhost.crt && echo "pwd is $(pwd)"
 
 python3 -m pip show requests certifi urllib3
 
-REQUESTS_CA_BUNDLE="$(pwd)/localhost.crt" CURL_CA_BUNDLE="$(pwd)/localhost.crt" XDMOD_API_TOKEN="$api_token" XDMOD_HOST="https://localhost:8080" python3 -m pytest --cov --cov-branch -vvs -o log_cli=true tests/ || true
+CURL_CA_BUNDLE="$(pwd)/localhost.crt" XDMOD_API_TOKEN="$api_token" XDMOD_HOST="https://localhost:8080" python3 -m pytest --cov --cov-branch -vvs -o log_cli=true tests/ || true
 
 mv .coverage ".coverage.${PYTHON_VERSION}.${XDMOD_VERSION}"
 
