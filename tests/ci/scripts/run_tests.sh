5197033aa0077d746bb10b07437676a65cc2b77f
RodneyCodess <rodneyga@buffalo.edu>
2026-06-10 11:24:12 -0400

    update

--- tests/ci/scripts/run_tests.sh
+++ tests/ci/scripts/run_tests.sh
@@ -50,6 +50,10 @@ echo "XDMOD_API_TOKEN=$api_token" > ${XDMOD_VERSION}-token
 
 echo " THIS IS THE API TOKEN !!!!! [$api_token]"
 
+ls -la localhost.crt && echo "pwd is $(pwd)"
+
+python3 -m pip show requests certifi urllib3
+
 REQUESTS_CA_BUNDLE="$(pwd)/localhost.crt" CURL_CA_BUNDLE="$(pwd)/localhost.crt" XDMOD_API_TOKEN="$api_token" XDMOD_HOST="https://localhost:8080" python3 -m pytest --cov --cov-branch -vvs -o log_cli=true tests/ || true
 
 mv .coverage ".coverage.${PYTHON_VERSION}.${XDMOD_VERSION}"
