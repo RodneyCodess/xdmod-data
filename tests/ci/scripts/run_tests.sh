63ada5e4c3108858424e7d3db51fc233d015d7b9
RodneyCodess <rodneyga@buffalo.edu>
2026-06-11 13:19:20 -0400

    update

--- tests/ci/scripts/run_tests.sh
+++ tests/ci/scripts/run_tests.sh
@@ -51,7 +51,7 @@ echo "XDMOD_API_TOKEN=$api_token" > ${XDMOD_VERSION}-token
 
 cat "$(pwd)/localhost.crt" >> "$(python3 -c 'import certifi; print(certifi.where())')"
 
-SSL_CERT_FILE="$(pwd)/localhost.crt" XDMOD_API_TOKEN="$api_token" XDMOD_HOST="https://localhost:8080" python3 -m pytest --cov --cov-branch -vvs -o log_cli=true tests/ || true
+XDMOD_API_TOKEN="$api_token" XDMOD_HOST="https://localhost:8080" python3 -m pytest --cov --cov-branch -vvs -o log_cli=true tests/
 
 mv .coverage ".coverage.${PYTHON_VERSION}.${XDMOD_VERSION}"
 
