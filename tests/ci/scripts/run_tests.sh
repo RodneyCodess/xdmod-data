68f7ada7d2ec6a165f247a8d1d356fc623300820
RodneyCodess <rodneyga@buffalo.edu>
2026-06-11 14:59:27 -0400

    update

--- tests/ci/scripts/run_tests.sh
+++ tests/ci/scripts/run_tests.sh
@@ -43,16 +43,18 @@ if [ "$PYTHON_VERSION" = "min-python" ]; then
 fi
 
 rest_token=$(
-    curl --cacert "$(pwd)/localhost.crt" -sS -X POST -c xdmod.cookie -d 'username=normaluser&password=normaluser' https://localhost:8080/rest/auth/login | 
+    CURL_CA_BUNDLE=localhost.crt curl -sS -X POST -c xdmod.cookie -d 'username=normaluser&password=normaluser' https://localhost:8080/rest/auth/login |
     jq -r '.results.token'
     )
 
-api_token=$(curl --cacert "$(pwd)/localhost.crt" -sS -X POST -b xdmod.cookie "https://localhost:8080/rest/users/current/api/token?token=$rest_token" | jq -r '.data.token')
+
+api_token=$(CURL_CA_BUNDLE=localhost.crt curl -sS -X POST -b xdmod.cookie "https://localhost:8080/rest/users/current/api/token?token=$rest_token" | jq -r '.data.token')
+
 
 echo "XDMOD_API_TOKEN=$api_token" > ${XDMOD_VERSION}-token
 
 cat "$(pwd)/localhost.crt" >> "$(python3 -c 'import certifi; print(certifi.where())')"
 
-REQUESTS_CA_BUNDLE=localhost.crt XDMOD_API_TOKEN="$api_token" XDMOD_HOST="https://localhost:8080" python3 -m pytest --cov --cov-branch -vvs -o log_cli=true tests/ || true
+REQUESTS_CA_BUNDLE=localhost.crt XDMOD_API_TOKEN="$api_token" XDMOD_HOST="https://localhost:8080" python3 -m pytest --cov --cov-branch -vvs -o log_cli=true tests/
 
 mv .coverage ".coverage.${PYTHON_VERSION}.${XDMOD_VERSION}"
