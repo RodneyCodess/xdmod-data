c5fdf95896934fdbae30d28b361dc1263d366b7d
RodneyCodess <rodneyga@buffalo.edu>
2026-06-30 12:05:59 -0400

    final update

--- tests/regression/test_datawarehouse_regression.py
+++ tests/regression/test_datawarehouse_regression.py
@@ -119,7 +119,10 @@ def test_get_raw_data(valid_dw, capsys, additional_params, number, csv_title):
         ),
     )
      # This PR added an extra job: https://github.com/ubccr/xdmod/pull/2176
-    if XDMOD_VERSION in ['xdmod-11-0', 'xdmod-11-0-dev']:
+    if (
+        XDMOD_VERSION in ['xdmod-11-0', 'xdmod-11-0-dev']
+        and csv_title == 'raw-data-every-1000-no-fields-no-filters.csv'
+    ):
         number = str(int(number) - 1)
     assert 'Got ' + number + ' rows...DONE' in capsys.readouterr().out
 
