b33d0203082cbccdfe59ce06e60f55e49bdecc90
RodneyCodess <rodneyga@buffalo.edu>
2026-07-13 14:46:24 -0400

    modified

--- tests/ci/scripts/run_tests.sh
+++ tests/ci/scripts/run_tests.sh
@@ -11,7 +11,9 @@ declare -A XDMOD_HOSTS=(
 )
 
 for py_version in "$MIN_PYTHON" "$MAX_PYTHON"; do
-    pyenv install -s "$py_version"
+    if command -v pyenv >/dev/null 2>&1; then
+        pyenv install -s "$py_version"
+    fi
     python$py_version -m venv /tmp/venv-$py_version
     source /tmp/venv-$py_version/bin/activate
 
@@ -30,55 +32,22 @@ for py_version in "$MIN_PYTHON" "$MAX_PYTHON"; do
     for xdmod_version in "${!XDMOD_HOSTS[@]}"; do
         host="${XDMOD_HOSTS[$xdmod_version]}"
 
-        rest_token=$(curl --cacert "localhost.crt" -sS -X POST -c xdmod.cookie -d 'username=normaluser&password=normaluser' $host/rest/auth/login | jq -r '.results.token')
-        api_token=$(curl --cacert "localhost.crt" -sS -X POST -b xdmod.cookie "$host/rest/users/current/api/token?token=$rest_token" | jq -r '.data.token')
+        docker exec "$xdmod_version" bash -c "openssl genrsa -rand /proc/cpuinfo:/proc/filesystems:/proc/interrupts:/proc/ioports:/proc/uptime 2048 > /etc/pki/tls/private/localhost.key"
+        docker cp tests/ci/artifacts/openssl.cnf "$xdmod_version":/root/openssl.cnf
+        docker exec "$xdmod_version" bash -c "XDMOD_CONTAINER=$xdmod_version openssl req -new -key /etc/pki/tls/private/localhost.key -x509 -sha256 -days 365 -set_serial $RANDOM -out /etc/pki/tls/certs/$xdmod_version.crt -config /root/openssl.cnf"
+        docker cp $xdmod_version:/etc/pki/tls/certs/$xdmod_version.crt .
+
+        rest_token=$(curl --cacert "$xdmod_version.crt" -sS -X POST -c xdmod.cookie -d 'username=normaluser&password=normaluser' $host/rest/auth/login | jq -r '.results.token')
+        api_token=$(curl --cacert "$xdmod_version.crt" -sS -X POST -b xdmod.cookie "$host/rest/users/current/api/token?token=$rest_token" | jq -r '.data.token')
 
         echo "XDMOD_API_TOKEN=$api_token" > ~/.xdmod-data-token
 
-        XDMOD_VERSION="$xdmod_version" XDMOD_HOST="$host" python3 -m pytest --cov --cov-branch --cov-append -vvs -o log_cli=true tests/
+        REQUESTS_CA_BUNDLE="$xdmod_version.crt" XDMOD_VERSION="$xdmod_version" XDMOD_HOST="$host" \
+            python3 -m pytest --cov --cov-branch --cov-append -vvs -o log_cli=true tests/
 
     done
-
-
-
 
     deactivate
 done
 
 python3 -m coverage report -m
-
-# if [ "$IS_MIN_PYTHON" = "true" ]; then
-#     export SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
-# fi
-
-# python3 -m venv $VENV
-# source $VENV/bin/activate
-
-# python3 -m pip install --upgrade pip
-# python3 -m pip install -e .[report]
-# python3 -m pip install --upgrade python-dotenv pytest pytest-cov
-
-# # force-install the oldest supported dependency versions
-# if [ "$IS_MIN_PYTHON" = "true" ]; then
-
-#     min_dependency_versions=$(awk \
-#         '/install_requires/ {flag=1} flag && !/install_requires/ && NF {print $0} flag && /^\[.*\]$/ {flag=0}' \
-#         setup.cfg | tr -d '\n' | sed 's/ >= /==/g'
-#     )
-#     python3 -m pip install --force-reinstall $min_dependency_versions
-# fi
-
-
-# # fetch API token
-# rest_token=$(curl --cacert "localhost.crt" -sS -X POST -c xdmod.cookie -d 'username=normaluser&password=normaluser' https://localhost:$PORT/rest/auth/login | jq -r '.results.token')
-# api_token=$(curl --cacert "localhost.crt" -sS -X POST -b xdmod.cookie "https://localhost:$PORT/rest/users/current/api/token?token=$rest_token" | jq -r '.data.token')
-
-# # write the token where the tests read it
-# echo "XDMOD_API_TOKEN=$api_token" > ~/.xdmod-data-token
-
-# # trust the cert for requests
-# cat "localhost.crt" >> "$(python3 -c 'import certifi; print(certifi.where())')"
-
-# REQUESTS_CA_BUNDLE=localhost.crt XDMOD_HOST="https://localhost:$PORT" python3 -m pytest --cov --cov-branch -vvs -o log_cli=true tests/
-
-# mv .coverage ".coverage.${PYTHON_VERSION}.${XDMOD_VERSION}"
